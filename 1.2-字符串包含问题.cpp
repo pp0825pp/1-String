2b49bc81219f3f24d42cf15a08e74089fd1ebcac
pp0825pp <905092043@qq.com>
2013-12-15 20:17:43 +0800

    Create 1.2-字符串包含问题.cpp
    题目：
       假设有两个由各种字母组成的字符串A和字符串B，其中字符串B中的字母数相对字符串A少。通过何种方法能最快的查出字符串B中的所有字母都包含在字符串A中呢？
       如：字符串A：ABCDEFGHLMNOPQRS；字符串B：DCGSRQPO 
          答案是true。

---
+++ 1.2-字符串包含问题.cpp
@@ -0,0 +1,41 @@
+//时间复杂度O(m+n)
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string strOne = "ABCDEFGHIJKHER";
+    string strTwo = "DFGHJAAWE";
+    
+    //辅助数组并清零
+    int hash[26] = {0};
+    //辅助数组中元素个数
+    int num = 0;
+    
+    //遍历短字符串 
+    for(int j = 0; j < strTwo.length(); j++)
+    {
+         int index = strTwo[j] - 'A';
+         if(hash[index] == 0)
+         {
+             hash[index] = 1;
+             num++;               
+         }     
+    }  
+    //遍历长字符串
+    for(int i = 0; i < strOne.length(); i++)
+    {
+         int index = strOne[i] - 'A';
+         if(hash[index] == 1)
+         {
+             hash[index] = 0;
+             num--;
+             if(num == 0)
+                break;               
+         }        
+    }  
+    if(num == 0)
+         cout<<"true"<<endl;
+    else
+         cout<<"false"<<endl;
+}
